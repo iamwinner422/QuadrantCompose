c531124096ff8ed91a98054b4cdc795ee28e7151
iamwinner422 <iamwinner422@gmail.com>
2024-11-02 15:40:53 +0000

    construction du premier cadrant

--- app/src/main/java/com/example/quadrantcompose/MainActivity.kt
+++ app/src/main/java/com/example/quadrantcompose/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.quadrantcompose.ui.theme.QuadrantComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuadrantComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    QuadrantApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +37,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun QuadrantApp(modifier: Modifier = Modifier){
+
+}
+
+@Composable
+fun TextComposable(modifier: Modifier = Modifier){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.background()
+    ) {
+        Text(
+            text = stringResource(R.string.title_1),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.description_1),
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     QuadrantComposeTheme {
-        Greeting("Android")
+        TextComposable()
     }
 }
--- app/src/main/java/com/example/quadrantcompose/ui/theme/Color.kt
+++ app/src/main/java/com/example/quadrantcompose/ui/theme/Color.kt
@@ -10,7 +10,3 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val FirstColor = Color(0xFFEADDFF)
-val SecondColor = Color(0xFFD0BCFF)
-val ThirdColor = Color(0xFFB69DF8)
-val FourthColor = Color(0xFFF6EDFF)
