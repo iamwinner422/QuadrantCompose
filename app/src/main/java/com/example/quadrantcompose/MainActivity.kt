2d9d4e510567256e2c81426e8aa8c81f2930663c
iamwinner422 <iamwinner422@gmail.com>
2024-11-02 15:55:01 +0000

    finition

--- app/src/main/java/com/example/quadrantcompose/MainActivity.kt
+++ app/src/main/java/com/example/quadrantcompose/MainActivity.kt
@@ -7,17 +7,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.quadrantcompose.ui.theme.QuadrantComposeTheme
@@ -38,23 +42,69 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun QuadrantApp(modifier: Modifier = Modifier){
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier.weight(1f)
+        ) {
+            ComposableInfo(
+                title = stringResource(R.string.title_1),
+                description = stringResource(R.string.description_1),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfo(
+                title = stringResource(R.string.title_2),
+                description = stringResource(R.string.description_2),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(
+            modifier = Modifier.weight(1f)
+        ) {
+            ComposableInfo(
+                title = stringResource(R.string.title_3),
+                description = stringResource(R.string.description_3),
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfo(
+                title = stringResource(R.string.title_4),
+                description = stringResource(R.string.description_4),
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
 
+    }
 }
 
 @Composable
-fun TextComposable(modifier: Modifier = Modifier){
+fun ComposableInfo(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+){
     Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.background()
+
     ) {
         Text(
-            text = stringResource(R.string.title_1),
+            text = title,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(16.dp)
         )
         Text(
-            text = stringResource(R.string.description_1),
+            text = description,
+            textAlign = TextAlign.Justify
         )
     }
 }
@@ -63,6 +113,6 @@ fun TextComposable(modifier: Modifier = Modifier){
 @Composable
 fun GreetingPreview() {
     QuadrantComposeTheme {
-        TextComposable()
+        QuadrantApp()
     }
 }
